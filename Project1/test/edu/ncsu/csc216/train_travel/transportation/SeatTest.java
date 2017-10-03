e661f8ef57e3337621ac817a9873d34cb9269ddf
nnbenven <nnbenven@ncsu.edu>
2017-10-03 11:18:10 -0400

    Recreated SeatTest to autogenerate methods

--- Project1/test/edu/ncsu/csc216/train_travel/transportation/SeatTest.java
+++ Project1/test/edu/ncsu/csc216/train_travel/transportation/SeatTest.java
@@ -21,8 +21,75 @@ public class SeatTest {
 	public void setUp() throws Exception {
 	}
 
+	/**
+	 * Test method for {@link edu.ncsu.csc216.train_travel.transportation.Seat#Seat(java.lang.String, int)}.
+	 */
 	@Test
-	public void test() {
+	public void testSeat() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.ncsu.csc216.train_travel.transportation.Seat#reserve()}.
+	 */
+	@Test
+	public void testReserve() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.ncsu.csc216.train_travel.transportation.Seat#release()}.
+	 */
+	@Test
+	public void testRelease() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.ncsu.csc216.train_travel.transportation.Seat#isReserved()}.
+	 */
+	@Test
+	public void testIsReserved() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.ncsu.csc216.train_travel.transportation.Seat#getTrainCarNumber()}.
+	 */
+	@Test
+	public void testGetTrainCarNumber() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.ncsu.csc216.train_travel.transportation.Seat#getLabel()}.
+	 */
+	@Test
+	public void testGetLabel() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.ncsu.csc216.train_travel.transportation.Seat#toString()}.
+	 */
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.ncsu.csc216.train_travel.transportation.Seat#printListOfSeats(edu.ncsu.csc216.train_travel.transportation.Seat[])}.
+	 */
+	@Test
+	public void testPrintListOfSeats() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.ncsu.csc216.train_travel.transportation.Seat#compareTo(edu.ncsu.csc216.train_travel.transportation.Seat)}.
+	 */
+	@Test
+	public void testCompareTo() {
 		fail("Not yet implemented");
 	}
 
