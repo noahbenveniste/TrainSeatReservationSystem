835b34878b4112a18fc6108bea96b9b2ed21a84b
nnbenven <nnbenven@ncsu.edu>
2017-10-07 22:02:33 -0400

    Added test class skeleton for Train that was forgotten

---
+++ Project1/test/edu/ncsu/csc216/train_travel/transportation/TrainTest.java
@@ -0,0 +1,144 @@
+/**
+ * 
+ */
+package edu.ncsu.csc216.train_travel.transportation;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Unit tests for Train
+ * @author Noah Benveniste
+ */
+public class TrainTest {
+
+	/**
+	 * Test method for {@link edu.ncsu.csc216.train_travel.transportation.Train#Train(int)}.
+	 */
+	@Test
+	public void testTrain() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.ncsu.csc216.train_travel.transportation.Train#numCars()}.
+	 */
+	@Test
+	public void testNumCars() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.ncsu.csc216.train_travel.transportation.Train#incComfortClassPassengers(int)}.
+	 */
+	@Test
+	public void testIncComfortClassPassengers() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.ncsu.csc216.train_travel.transportation.Train#incEconomyClassPassengers(int)}.
+	 */
+	@Test
+	public void testIncEconomyClassPassengers() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.ncsu.csc216.train_travel.transportation.Train#incBicyclePassengers(int)}.
+	 */
+	@Test
+	public void testIncBicyclePassengers() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.ncsu.csc216.train_travel.transportation.Train#getSeatFor(int, int, int)}.
+	 */
+	@Test
+	public void testGetSeatForIntIntInt() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.ncsu.csc216.train_travel.transportation.Train#getSeatFor(int, java.lang.String)}.
+	 */
+	@Test
+	public void testGetSeatForIntString() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.ncsu.csc216.train_travel.transportation.Train#hasComfortClassRoomFor(int)}.
+	 */
+	@Test
+	public void testHasComfortClassRoomFor() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.ncsu.csc216.train_travel.transportation.Train#hasEconomyClassRoomFor(int)}.
+	 */
+	@Test
+	public void testHasEconomyClassRoomFor() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.ncsu.csc216.train_travel.transportation.Train#hasBicycleCarRoomFor(int)}.
+	 */
+	@Test
+	public void testHasBicycleCarRoomFor() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.ncsu.csc216.train_travel.transportation.Train#openSecondClassSeats()}.
+	 */
+	@Test
+	public void testOpenSecondClassSeats() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.ncsu.csc216.train_travel.transportation.Train#openFirstClassSeats()}.
+	 */
+	@Test
+	public void testOpenFirstClassSeats() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.ncsu.csc216.train_travel.transportation.Train#isBicycleCar(int)}.
+	 */
+	@Test
+	public void testIsBicycleCar() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.ncsu.csc216.train_travel.transportation.Train#isSecondClassCar(int)}.
+	 */
+	@Test
+	public void testIsSecondClassCar() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.ncsu.csc216.train_travel.transportation.Train#isFirstClassCar(int)}.
+	 */
+	@Test
+	public void testIsFirstClassCar() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.ncsu.csc216.train_travel.transportation.Train#getCarSeatMap(int)}.
+	 */
+	@Test
+	public void testGetCarSeatMap() {
+		fail("Not yet implemented");
+	}
+
+}
