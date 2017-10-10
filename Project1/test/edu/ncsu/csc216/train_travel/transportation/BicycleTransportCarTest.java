742eca43576049cb1c2537f31bc553c4c4ef5fe0
nnbenven <nnbenven@ncsu.edu>
2017-10-09 20:11:07 -0400

    Completed unit tests for BicycleTransportCar

--- Project1/test/edu/ncsu/csc216/train_travel/transportation/BicycleTransportCarTest.java
+++ Project1/test/edu/ncsu/csc216/train_travel/transportation/BicycleTransportCarTest.java
@@ -9,53 +9,105 @@ import org.junit.Test;
  * @author Noah Benveniste
  */
 public class BicycleTransportCarTest {
+	/** Error message */
+	private static final String SEAT_ERROR_MESSAGE = "Cannot reserve seats in a bicycle transport car";
 
 	/**
-	 * Test method for {@link edu.ncsu.csc216.train_travel.transportation.BicycleTransportCar#openSeatsLeft()}.
-	 */
-	@Test
-	public void testOpenSeatsLeft() {
-		fail("Not yet implemented");
-	}
-
-	/**
-	 * Test method for {@link edu.ncsu.csc216.train_travel.transportation.BicycleTransportCar#seatFor(int, int)}.
-	 */
-	@Test
-	public void testSeatForIntInt() {
-		fail("Not yet implemented");
-	}
-
-	/**
-	 * Test method for {@link edu.ncsu.csc216.train_travel.transportation.BicycleTransportCar#seatFor(java.lang.String)}.
-	 */
-	@Test
-	public void testSeatForString() {
-		fail("Not yet implemented");
-	}
-
-	/**
-	 * Test method for {@link edu.ncsu.csc216.train_travel.transportation.BicycleTransportCar#getSeatMap()}.
-	 */
-	@Test
-	public void testGetSeatMap() {
-		fail("Not yet implemented");
-	}
-
-	/**
-	 * Test method for {@link edu.ncsu.csc216.train_travel.transportation.BicycleTransportCar#BicycleTransportCar(int)}.
+	 * Test method for constructor
 	 */
 	@Test
 	public void testBicycleTransportCar() {
-		fail("Not yet implemented");
+		//Test valid middle value construction
+		BicycleTransportCar car1 = new BicycleTransportCar(6);
+		assertNotNull(car1);
+		
+		//Test valid lower boundary value construction
+		BicycleTransportCar car2 = new BicycleTransportCar(1);
+		assertNotNull(car2);
+		
+		//Test valid upper boundary value construction 
+		BicycleTransportCar car3 = new BicycleTransportCar(12);
+		assertNotNull(car3);
+		
+		//Test invalid lower boundary value construction
+		BicycleTransportCar car4 = null;
+		try {
+			car4 = new BicycleTransportCar(0);
+			fail();
+		} catch (IllegalArgumentException e) {
+			assertEquals("Invalid car number", e.getMessage());
+			assertNull(car4);
+		}
+		
+		//Test invalid lower boundary value construction
+		BicycleTransportCar car5 = null;
+		try {
+			car5 = new BicycleTransportCar(13);
+			fail();
+		} catch (IllegalArgumentException e) {
+			assertEquals("Invalid car number", e.getMessage());
+			assertNull(car5);
+		}
 	}
 
 	/**
-	 * Test method for {@link edu.ncsu.csc216.train_travel.transportation.BicycleTransportCar#getCapacity()}.
+	 * Test method for getCapacity()
 	 */
 	@Test
 	public void testGetCapacity() {
-		fail("Not yet implemented");
+		assertEquals(20, BicycleTransportCar.getCapacity());
 	}
 
+	/**
+	 * Test method for openSeatsLeft()
+	 */
+	@Test
+	public void testOpenSeatsLeft() {
+		//This method is not applicable to BicycleTransportCars, should always return 0
+		BicycleTransportCar car = new BicycleTransportCar(1);
+		assertEquals(0, car.openSeatsLeft());
+	}
+
+	/**
+	 * Test method for seatFor(int, int)
+	 */
+	@Test
+	public void testSeatForIntInt() {
+		//This method is not applicable to BicycleTransportCars, should throw an IAE
+		BicycleTransportCar car = new BicycleTransportCar(1);
+		Seat seat1 = null;
+		try {
+			seat1 = car.seatFor(0, 0);
+		} catch (IllegalArgumentException e) {
+			assertNull(seat1);
+			assertEquals(SEAT_ERROR_MESSAGE, e.getMessage());
+		}
+		
+	}
+
+	/**
+	 * Test method for seatFor(String)
+	 */
+	@Test
+	public void testSeatForString() {
+		//This method is not applicable to BicycleTransportCars, should throw an IAE
+		BicycleTransportCar car = new BicycleTransportCar(1);
+		Seat seat1 = null;
+		try {
+			seat1 = car.seatFor("1A");
+		} catch (IllegalArgumentException e) {
+			assertNull(seat1);
+			assertEquals(SEAT_ERROR_MESSAGE, e.getMessage());
+		}
+	}
+
+	/**
+	 * Test method for getSeatMap()
+	 */
+	@Test
+	public void testGetSeatMap() {
+		BicycleTransportCar car = new BicycleTransportCar(1);
+		String expected = "\nBicycle Transport Car #1\n Seats cannot be reserved.";
+		assertEquals(expected, car.getSeatMap());
+	}
 }
